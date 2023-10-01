289261440b91dfef19d95ce28a5a65eebd5bbaf4
sebalopezx <se.lopezn@gmail.com>
2023-10-01 18:48:18 -0300

    6. test produccion fix database_url

--- Project/settings.py
+++ Project/settings.py
@@ -115,7 +115,7 @@ WSGI_APPLICATION = 'Project.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.path.join(BASE_DIR, 'db.sqlite3'),
+        default=os.environ.get('DATABASE_URL')
     )
     # {
     #     'ENGINE': 'django.db.backends.sqlite3',
