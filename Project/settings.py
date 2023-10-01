3b128ab0ce668cc1b88fb9542b43115fb652fa07
sebalopezx <se.lopezn@gmail.com>
2023-10-01 20:11:29 -0300

    8. test debug True.

--- Project/settings.py
+++ Project/settings.py
@@ -31,8 +31,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = 'RENDER' not in os.environ
+DEBUG = True
+# DEBUG = 'RENDER' not in os.environ
 
 
 ALLOWED_HOSTS = []
@@ -113,17 +113,20 @@ WSGI_APPLICATION = 'Project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# if 'DABATASE_URL' in os.environ:
+
 DATABASES = {
     'default': dj_database_url.config(
         # default=os.environ.get('DATABASE_URL')
         default='postgresql://postgres:postgres@localhost/postgres'
     )
-    # {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
 }
-
+# DATABASES = {
+#     'default':{
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
